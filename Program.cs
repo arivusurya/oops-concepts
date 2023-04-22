4db7b3a3b7a8127c0a5e90d1faa34381702e77ff
arivusurya <arivusurya.3@gmail.com>
2023-04-22 08:39:45 +0530

    examplae for the class and object

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+
+namespace oops_concepts{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            myfirstclass name = new myfirstclass();
+            int value = name.add(10,30);
+            Console.WriteLine(value);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ myfirstclass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+
+namespace oops_concepts
+{
+    public class myfirstclass
+    {
+        
+        public int add(int  x ,int y){
+            int z= x+y;
+            return z;
+        }
+    }
+}
